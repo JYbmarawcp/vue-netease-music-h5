016e549f497e2d54e0b7558cc52c78c721dcb057
JYbmarawcp <fjh2333@gmail.com>
2020-09-04 17:44:07 +0800

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import musicModule from './modules/music'
+import createLogger from 'vuex/dist/logger'
 
 Vue.use(Vuex)
+const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-  }
+    music: musicModule
+  },
+  plugins: debug ? [createLogger()] : []
 })
