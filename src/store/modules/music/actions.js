25d5750ee59e861da62d43669977e7cfa1350beb
JYbmarawcp <fjh2333@gmail.com>
2020-10-15 19:31:27 +0800

    vuex

--- src/store/modules/music/actions.js
+++ src/store/modules/music/actions.js
@@ -1,5 +1,8 @@
 import { getSongImg } from "@/utils"
 
+// Action 函数接受一个与 store 实例具有相同方法和属性的 context 对象，
+// 因此你可以调用 context.commit 提交一个 mutation，或者通过 context.state 和 context.getters 来获取 state 和 getters。
+// 实践中，我们会经常用到 ES2015 的 参数解构 来简化代码
 export default {
   // 整合歌曲信息 并开始播放
   async startSong({ commit }, rawSong) {
